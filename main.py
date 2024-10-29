16aa00632b96aad58fd18c547a15731965ec933c
[PedroPabloromerotorres](https://github.com/PedroPabloromerotorres) <186082673+PedroPabloromerotorres@users.noreply.github.com>
2024-10-29 12:43:36 -0500

    solucion del ejercicio circulos

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-name = input("enter your name: ")
-print(f"""hello, {name}""")
+#Escriba un programa que reciba como entrada el radio de un círculo y entregue como salida su perímetro y su área:
+
+#Ingrese el radio: 5
+#Perimetro: 31.4
+#Área: 78.5
+
+radio = float (input("What is the radio station?: "))
+
+perimetro = 2 * 3.14 * radio
+
+area = 3.14 * radio * radio
+
+print (f"el perimetro del circulo es equivalente a: {perimetro}")
+
+print (f"la área del circulo es equivalente a: {area}")
